66349b93f6ebcbc86aca65a1962e7fa1507cf4a6
Iordanis Fostiropoulos <fostiropoulos@users.noreply.github.com>
2024-06-30 12:54:37 -0700

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from time import sleep
 import aiofiles
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import StreamingResponse
+from fastapi.responses import FileResponse, StreamingResponse
 import uvicorn
 
 app = FastAPI()
@@ -36,26 +36,41 @@ async def stream_markdown(filename: str):
 
 
 @app.get("/html/{filename}")
-async def stream_markdown(filename: str):
+async def stream_html(filename: str):
     file_path = os.path.join(HTML_DIR, filename)
     print(file_path)
 
-    if not file_path.endswith(".html"):
-        raise HTTPException(status_code=400, detail="File must have .md extension")
+    # if not file_path.endswith(".html"):
+    #     raise HTTPException(status_code=400, detail="File must have .md extension")
 
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
+    # if not os.path.exists(file_path):
+    #     raise HTTPException(status_code=404, detail="File not found")
 
-    async def file_iterator():
-        async with aiofiles.open(file_path, mode="r", encoding="utf-8") as file:
-            while True:
-                char = await file.read(1)
-                sleep(0.00)
-                if not char:
-                    break
-                yield char.encode("utf-8")
+    def file_iterator():
+        return open(file_path, mode="r").read()
 
-    return StreamingResponse(file_iterator(), media_type="text/html")
+        # async with aiofiles.open(file_path, mode="r", encoding="utf-8") as file:
+        #     while True:
+        #         char = await file.read(1)
+        #         sleep(0.00)
+        #         if not char:
+        #             break
+        #         yield char.encode("utf-8")
+
+    return StreamingResponse(open(file_path, mode="r").read(), media_type="text/html")
+
+@app.get("/image/{filename}")
+async def stream_image(filename: str):
+    file_path = os.path.join(HTML_DIR, filename)
+    print(file_path)
+
+    # if not file_path.endswith(".html"):
+    #     raise HTTPException(status_code=400, detail="File must have .md extension")
+
+    # if not os.path.exists(file_path):
+    #     raise HTTPException(status_code=404, detail="File not found")
+
+    return FileResponse(file_path) # (open(file_path, mode="rb").read(), media_type="image/png")
 
 
 @app.get("/")
