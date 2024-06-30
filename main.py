6722e282605b31e54e14c854c20f40700323a39e
JP <jp.guerra@checkpoint-ai.com>
2024-06-30 13:01:03 -0700

    changes jp

--- main.py
+++ main.py
@@ -12,29 +12,6 @@ MARKDOWN_DIR = os.getenv("MARKDOWN_DIR", "./mock documents")
 HTML_DIR = os.getenv("HTML_DIR", "./html")
 
 
-@app.get("/markdown/{filename}")
-async def stream_markdown(filename: str):
-    file_path = os.path.join(MARKDOWN_DIR, filename)
-    print(file_path)
-
-    if not file_path.endswith(".md"):
-        raise HTTPException(status_code=400, detail="File must have .md extension")
-
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
-
-    async def file_iterator():
-        async with aiofiles.open(file_path, mode="r", encoding="utf-8") as file:
-            while True:
-                char = await file.read(1)
-                sleep(0.01)
-                if not char:
-                    break
-                yield char.encode("utf-8")
-
-    return StreamingResponse(file_iterator(), media_type="text/markdown")
-
-
 @app.get("/html/{filename}")
 async def stream_markdown(filename: str):
     file_path = os.path.join(HTML_DIR, filename)
